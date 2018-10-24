f331d3320e390f6e57695acfa12667962119b110
sethvuz <diaquinus@gmail.com>
2018-10-23 22:00:47 -0300

    Funções de cadastro e login.

---
+++ app/src/main/java/com/honda/debrincar/Activities/CadastroUser.java
@@ -0,0 +1,15 @@
+package com.honda.debrincar.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.honda.debrincar.R;
+
+public class CadastroUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_user);
+    }
+}
