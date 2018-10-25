9f67a3dbf6b6047f9337b1ce7d3e38904167125f
sethvuz <diaquinus@gmail.com>
2018-10-25 02:11:01 -0300

    Funções de cadastro e login.

--- app/src/main/java/com/honda/debrincar/Activities/PrincipalAnuncios.java
+++ app/src/main/java/com/honda/debrincar/Activities/PrincipalAnuncios.java
@@ -1,15 +1,76 @@
 package com.honda.debrincar.Activities;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.honda.debrincar.R;
 
 public class PrincipalAnuncios extends AppCompatActivity {
 
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+    private RecyclerView listaAnuncios;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal_anuncios);
+
+        drawerLayout = findViewById(R.id.drawable_layout);
+        navigationView = findViewById(R.id.navegation_view);
+        View headView = navigationView.inflateHeaderView(R.layout.menu_nav_header);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                UserMenuSelector(menuItem);
+
+                return false;
+            }
+        });
+    }
+
+    private void UserMenuSelector(MenuItem menuItem) {
+
+        switch (menuItem.getItemId()){
+            case R.id.nav_anuncios:
+                Toast.makeText(this, "Anúncios", Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.nav_ins_anuncios:
+                Toast.makeText(this, "Inserir Anúncio", Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.nav_favoritos:
+                Toast.makeText(this, "Favoritos", Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.nav_seguidores:
+                Toast.makeText(this, "Seguidores", Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.nav_minha_conta:
+                Toast.makeText(this, "Minha Conta", Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.nav_config:
+                Toast.makeText(this, "Configurações", Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.nav_logout:
+                Toast.makeText(this, "Sair", Toast.LENGTH_LONG).show();
+                break;
+
+                default:
+                    break;
+        }
+
     }
 }
